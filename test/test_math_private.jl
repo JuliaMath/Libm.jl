d4787bf9727c9966961a56d30a9d4889ab8159a2
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-08-20 16:12:19 +1000

    =Test Float64 wordsplitting/merging

--- test/test_math_private.jl
+++ test/test_math_private.jl
@@ -18,13 +18,16 @@ using Libm: highword, lowword, combinewords
 	@test lowword(NaN) == 0x0000_0000
 	@test isnan(combinewords(0x7ff8_0000, 0x0000_0000))
 
+	@test highword(1.3) == 0x3ff4_cccc
+	@test lowword(1.3) == 0xcccc_cccd
+	@test combinewords(0x3ff4_cccc, 0xcccc_cccd) == 1.3
+
+
 	srand(1)
 	for testround in 1:1000
 		x = rand(Float64)
-		@test combinewords(highword(x), lowword(x)) == x 
+		@test combinewords(highword(x), lowword(x)) == x
 	end
-
-
 
 
 end
