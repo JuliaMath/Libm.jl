0657f9050c045a41bddf20d8d2bc0978c2e65589
Lyndon White <oxinabox@ucc.asn.au>
2016-08-20 15:57:09 +0800

    Revert "=High and Low words now the right way round"
    This reverts commit a8ba13091362fe2d1b5913ffdc3a371dbb666fee.

--- test/test_math_private.jl
+++ test/test_math_private.jl
@@ -1,36 +1,33 @@
 using Base.Test
 using Libm: highword, lowword, combinewords
 
-const fuzztest = true
-
 @testset "Word Splitting" begin
-	
-	@test lowword(0.0) == 0x0000_0000
 	@test highword(0.0) == 0x0000_0000
+	@test lowword(0.0) == 0x0000_0000
 	@test combinewords(0x0000_0000, 0x0000_0000) == 0.0
 
-	@test lowword(2.0) == 0x4000_0000
-	@test highword(2.0) == 0x0000_0000
-	@test combinewords(0x0000_0000, 0x4000_0000) == 2.0
+	@test highword(2.0) == 0x4000_0000
+	@test lowword(2.0) == 0x0000_0000
+	@test combinewords(0x4000_0000, 0x0000_0000) == 2.0
 
-	@test lowword(-2.0) == 0xc000_0000
-	@test highword(-2.0) == 0x0000_0000
-	@test combinewords(0x0000_0000, 0xc000_0000) == -2.0
+	@test highword(-2.0) == 0xc000_0000
+	@test lowword(-2.0) == 0x0000_0000
+	@test combinewords(0xc000_0000, 0x0000_0000) == -2.0
 
-	@test lowword(NaN) == 0x7ff8_0000
-	@test highword(NaN) == 0x0000_0000
-	@test isnan(combinewords(0x0000_0000, 0x7ff8_0000))
+	@test highword(NaN) == 0x7ff8_0000
+	@test lowword(NaN) == 0x0000_0000
+	@test isnan(combinewords(0x7ff8_0000, 0x0000_0000))
 
-	@test lowword(1.3) == 0x3ff4_cccc
-	@test highword(1.3) == 0xcccc_cccd
-	@test combinewords(0xcccc_cccd, 0x3ff4_cccc) == 1.3
+	@test highword(1.3) == 0x3ff4_cccc
+	@test lowword(1.3) == 0xcccc_cccd
+	@test combinewords(0x3ff4_cccc, 0xcccc_cccd) == 1.3
 
-	if fuzztest
-		srand(1)
-		for testround in 1:1000
-			x = rand(Float64)
-			@test combinewords(highword(x), lowword(x)) == x
-		end
+
+	srand(1)
+	for testround in 1:1000
+		x = rand(Float64)
+		@test combinewords(highword(x), lowword(x)) == x
 	end
+
 
 end
