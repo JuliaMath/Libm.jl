a8ba13091362fe2d1b5913ffdc3a371dbb666fee
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-08-20 17:13:28 +1000

    =High and Low words now the right way round

--- src/math_private.jl
+++ src/math_private.jl
@@ -1,12 +1,20 @@
 #Relates to OpenLibm: src/math_private, however little of that code can be transfered as it uses C unions.
 
+using Base: @pure
 
+"""Return the least significant word of d.
+Coresponds to OpenLibm GET_LOW_WORD(ret,d)"""
+@inline @pure lowword(d::Float64) = (reinterpret(UInt64, d) >> 32) % UInt32
 
-function highword(d::Float64)
-	(reinterpret(UInt64, d) >> 32) % UInt32
+"""Returns the most significant word of d
+Coresponds to OpenLibm GET_HIGH_WORD(ret,d)
+"""
+@inline @pure highword(d::Float64) = reinterpret(UInt64, d) % UInt32
 
-function lowword(d::Float64)
-	reinterpret(UInt64, d) % UInt32
-
-
-
+"""
+Combined the high and low words, to make a Float64.
+Coresponds to OpenLibm INSERTWORDS(ret, hw, lw)
+"""
+@inline @pure function combinewords(hw::UInt32, lw::UInt32)::Float64
+	reinterpret(Float64, (lw % UInt64) << 32 + hw)
+end
