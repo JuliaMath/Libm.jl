fe4e01d2c8522a43e9f883e0661a56e596b96541
Mus M <mus-m@outlook.com>
2016-08-21 13:56:35 -0400

    fix scalbn and test

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,7 @@
 # Get the more significant 32 bit int from a double
 function get_high_word(d::Float64)
     u = reinterpret(UInt64, d)
-    return (u>>32)%UInt32
+    return (u>>32) % UInt32
 end
 
 # Set the less significant 32 bits of a double from an int
@@ -9,30 +9,29 @@ function set_low_word(d::Float64, lo::UInt32)
     return reinterpret(Float64, reinterpret(UInt64, d) & 0xffffffff00000000 | lo)
 end
 
+# Scale number x*2^n
 function scalbn(x::Float64, n::Int)
-    y = x
     if n > 1023
-        y *= 0x1p1023
+        x *= 0x1p1023
         n -= 1023;
         if n > 1023
-            y *= 0x1p1023
+            x *= 0x1p1023
             n -= 1023;
             if n > 1023
                 n = 1023
              end
         end
     elseif n < -1022
-        y *= 0x1p-1022
+        x *= 0x1p-1022
         n += 1022
         if n < -1022
-            y *= 0x1p-1022
+            x *= 0x1p-1022
             n += 1022
             if n < -1022
                 n = -1022
             end
         end
     end
-    u = reinterpret(Float64, (0x3ff+n)<<52%UInt64)
-    x = y * u
-    return x
+    u = reinterpret(Float64, (0x3ff+n) % UInt64 << 52)
+    return x*u
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,29 @@ using Libm
 using Base.Test
 
 include("./erf.jl")
+
+
+@testset "scalbn tests" begin
+    @test Libm.scalbn(1.0,0) == 0x1p0
+    @test Libm.scalbn(2.0,0) == 0x1p1 
+    @test Libm.scalbn(1.0,8) == 0x1p8
+    @test Libm.scalbn(2.0,8) == 0x1p9
+    @test Libm.scalbn(-1.0,0) == -0x1p0
+    @test Libm.scalbn(-2.0,0) == -0x1p1
+    @test Libm.scalbn(-1.0,8) == -0x1p8
+    @test Libm.scalbn(-2.0,8) == -0x1p9
+
+    @test Libm.scalbn(1.0,1023) == 0x1p1023 
+    @test Libm.scalbn(1.0,-1022) == 0x1p-1022
+
+    # ch
+    @test Libm.scalbn(1.0,1024) == Inf
+    @test Libm.scalbn(1.0,1024+1023) == Inf
+    @test Libm.scalbn(1.0,1024+1023+1023) == Inf
+    @test Libm.scalbn(1.0,1024+1023+1023) == Inf
+
+    @test Libm.scalbn(1.0,-1023) == 0x1p-1023
+    @test Libm.scalbn(1.0,-1023-1022) ==  0x0p0
+    @test Libm.scalbn(1.0,-1023-1022-1022)== 0x0p0
+    @test Libm.scalbn(1.0,-1023-1022-1022-1022)== 0x0p0
+end
