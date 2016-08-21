9756f572719594eafb1bab74bc8c7fe98da9bc97
Mus M <mus-m@outlook.com>
2016-08-21 14:23:40 -0400

    fd

--- src/Libm.jl
+++ src/Libm.jl
@@ -2,6 +2,8 @@ module Libm
 
 using Base.Math.@horner
 
+export _erf, _erfc
+
 include("utils.jl")
 include("erf.jl")
 
--- test/erf.jl
+++ test/erf.jl
@@ -1,35 +1,31 @@
-
-
 @testset "erf tests" begin
-    @test isnan(Base.erf.(NaN))
-    @test Libm.erf.(Inf) == 1.0
-    @test Libm.erf.(-Inf) == -1.0
+    @test isnan(erf.(NaN))
+    @test _erf.(Inf) == 1.0
+    @test _erf.(-Inf) == -1.0
     s = linspace(-0.84375,0.84375,100)
-    @test Base.erf.(s) == Libm.erf.(s)
+    @test erf.(s) == _erf.(s)
     s = linspace(-2e-28,2e-28,100)
-    @test Base.erf.(s) == Libm.erf.(s)
+    @test erf.(s) == _erf.(s)
     s = linspace(-0.84375,0.84375,100)
-    @test Base.erf.(s) == Libm.erf.(s)
+    @test erf.(s) == _erf.(s)
     s = linspace(0.84375, 6, 100)
-    @test Base.erf.(s) ==  Libm.erf.(s)
+    @test erf.(s) ==  _erf.(s)
     s = linspace(-6,-0.84375, 100);
-    @test Base.erf.(s) ==  Libm.erf.(s)
+    @test erf.(s) ==  _erf.(s)
 end
 
-
-
 @testset "erfc tests" begin
-    @test isnan(Base.erfc.(NaN))
-    @test Libm.erfc.(Inf) == 0.0
-    @test Libm.erfc.(-Inf) == 2.0
+    @test isnan(erfc.(NaN))
+    @test _erfc.(Inf) == 0.0
+    @test _erfc.(-Inf) == 2.0
     s = linspace(-0.84375,0.84375,100)
-    @test Base.erfc.(s) == Libm.erfc.(s)
+    @test erfc.(s) == _erfc.(s)
     s = linspace(-2e-56,2e-56,100)
-    @test Base.erfc.(s) == Libm.erfc.(s)
+    @test erfc.(s) == _erfc.(s)
     s = linspace(-0.25,0.25,100)
-    @test Base.erfc.(s) == Libm.erfc.(s)
+    @test erfc.(s) == _erfc.(s)
     s = linspace(0.84375, 28, 100)
-    @test Base.erfc.(s) ==  Libm.erfc.(s)
+    @test erfc.(s) ==  _erfc.(s)
     s = linspace(-28,-0.84375, 100);
-    @test Base.erfc.(s) ==  Libm.erfc.(s)
+    @test erfc.(s) ==  _erfc.(s)
 end
