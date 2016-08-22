a22aad4442ef014fe979b6a3121fda4a4043b1eb
Simon Byrne <simonbyrne@gmail.com>
2016-08-22 20:22:35 +0100

    add tests

---
+++ test/log.jl
@@ -0,0 +1,24 @@
+@testset for llog in (Libm.log_tang,)
+    for T in (Float32, Float64)
+        @test isnan(llog(T(NaN)))
+        @test llog(T(Inf)) == Inf
+        @test llog(T(0)) == -Inf
+        s = logspace(T(-10),T(10),1000)
+        @test Base.log.(s) ≈ llog.(s)
+        @test_throws DomainError llog(T(-1))
+    end
+end
+
+@testset for llog1p in (Libm.log1p_tang,)
+    for T in (Float32, Float64)
+        @test isnan(llog1p(T(NaN)))
+        @test llog1p(T(Inf)) == Inf
+        @test llog1p(T(0)) == 0
+        @test llog1p(T(-1)) == -Inf
+        s = logspace(T(-10),T(10),1000)
+        @test Base.log1p.(s) ≈ llog1p.(s)
+        s = -logspace(T(-10),T(0),1000)
+        @test Base.log1p.(s) ≈ llog1p.(s)
+        @test_throws DomainError llog1p(T(-2))
+    end
+end
