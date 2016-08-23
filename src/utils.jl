6e043d47e436bee52e60b4df577ccb1767c5b5d3
Simon Byrne <simonbyrne@gmail.com>
2016-08-23 21:27:49 +0100

    Rename lowword! as it is not mutating

--- src/utils.jl
+++ src/utils.jl
@@ -1,14 +1,18 @@
 """
-Get the more significant 32 bit int from a double.
-Corresponds to GET_HIGH_WORD in musl
+    highword(d::Float64)
+
+Get the most significant 32 bits as a `UInt32` from `d`.
+Corresponds to `GET_HIGH_WORD` in musl
 """
 highword(d::Float64) = (reinterpret(UInt64, d) >> 32) % UInt32
 
 """
-Set the less significant 32 bits of the double d to the unsigned 32 bit integer lo
-Corresponds to SET_LOW_WORD in musl
+    setlowword(d::Float64, lo::UInt32)
+
+Returns the least significant 32 bits of `d` to `lo`.
+Corresponds to `SET_LOW_WORD` in musl
 """
-lowword!(d::Float64, lo::UInt32) = reinterpret(Float64, reinterpret(UInt64, d) & 0xffffffff00000000 | lo)
+setlowword(d::Float64, lo::UInt32) = reinterpret(Float64, reinterpret(UInt64, d) & 0xffff_ffff_0000_0000 | lo)
 
 
 # determine if hardware FMA is available
