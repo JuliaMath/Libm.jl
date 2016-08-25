a1f41289e08858110f6fffb711f84103e8b40433
Mus M <mus-m@outlook.com>
2016-08-24 20:05:05 -0400

    Remove unecessary conversion

--- bench/bench.jl
+++ bench/bench.jl
@@ -23,18 +23,18 @@ xx_trig = linspace(-100, 100, 50_000_000)
 xx_hyp = linspace(-15, 15, 50_000_000)
 
 const micros = Dict(
-    "exp"   => xx_exp,
-    "exp2"  => xx_exp,
-    "exp10" => xx_exp,
-    "expm1" => xx_sm,
-    "log"   => xx_log,
-    "log10" => xx_log,
-    "log1p" => xx_sm,
-    "sin"   => xx_trig,
-    "cos"   => xx_trig,
-    "tan"   => xx_trig,
-    "sinh"  => xx_hyp,
-    "cosh"  => xx_hyp,
+    # "exp"   => xx_exp,
+    # "exp2"  => xx_exp,
+    # "exp10" => xx_exp,
+    # "expm1" => xx_sm,
+    # "log"   => xx_log,
+    # "log10" => xx_log,
+    # "log1p" => xx_sm,
+    # "sin"   => xx_trig,
+    # "cos"   => xx_trig,
+    # "tan"   => xx_trig,
+    # "sinh"  => xx_hyp,
+    # "cosh"  => xx_hyp,
     "tanh"  => xx_hyp
     )
 
@@ -60,3 +60,68 @@ for f in sort(collect(keys(micros)))
     # println(result[f]["Base"])
     # println()
 end
+
+# using Libm
+# using BenchmarkTools
+
+# const bench = ("Base","Libm")
+# const suite = BenchmarkGroup()
+
+# for n in bench
+#     suite[n] = BenchmarkGroup([n])
+# end
+# srand(100)
+
+# x1 = linspace(708.4, 709.7, 10000)
+# x2 = -linspace(708.4, 709.7, 10000)
+# x3 = linspace(0.5*log(2),1.5*log(2), 10000)
+# x4 = -linspace(0.5*log(2),1.5*log(2), 10000)
+# x5 = linspace(1.5*log(2),10, 10000)
+# x6 = -linspace(1.5*log(2),10, 10000)
+# x7 = linspace(2.0^-28,2.0^-27, 10000)
+# x8 = -linspace(2.0^-28,2.0^-27, 10000)
+# x9 = linspace(1.0, 10.0, 1000)
+# x10 = -linspace(1.0, 10.0, 1000)
+# x11 = 40000*(rand(2_000_000)-0.5)
+# xx_exp = union(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11)
+# xx_sm = logspace(-16,0,2_000_000)
+# xx_log = 4000*rand(2_000_000)
+# xx_trig = linspace(-100, 100, 50_000_000)
+# xx_hyp = linspace(-15, 15, 50_000_000)
+
+# const micros = Dict(
+#     # "exp"   => xx_exp,
+#     # "exp2"  => xx_exp,
+#     # "exp10" => xx_exp,
+#     # "expm1" => xx_sm,
+#     # "log"   => xx_log,
+#     # "log10" => xx_log,
+#     # "log1p" => xx_sm,
+#     # "sin"   => xx_trig,
+#     # "cos"   => xx_trig,
+#     # "tan"   => xx_trig,
+#     # "sinh"  => xx_hyp,
+#     # "cosh"  => xx_hyp,
+#     "tanh"  => xx_hyp
+#     )
+# for n in bench
+#     for (f,v) in micros
+#         suite[n][f] = BenchmarkGroup([f])
+#         n == "Libm" ? fun = Symbol("x",f) : fun = Symbol(f)
+#         suite[n][f] = @benchmarkable $fun.($v)
+#     end
+# end
+# tune!(suite)
+# results = run(suite)
+
+# for f in sort(collect(keys(micros)))
+#     println()
+#     print_with_color(:magenta, string(f, " benchmark\n"))
+#     print_with_color(:blue, "median ratio Libm/Base\n")
+#     println(ratio(median(results["Libm"][f]), median(results["Base"][f])))
+#     println()
+#     # print_with_color(:blue, "details Libm/Base\n")
+#     # println(result[f]["Libm"])
+#     # println(result[f]["Base"])
+#     # println()
+# end
