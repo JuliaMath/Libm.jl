4527927fd9a15351660cc81d74feb58f9dbcbad2
Simon Byrne <simonbyrne@gmail.com>
2016-08-27 22:02:33 +0100

    Add erf/erfc for Float32 (#14)
    Extend erf and erfc to Float32 using generics.

--- src/Libm.jl
+++ src/Libm.jl
@@ -1,15 +1,9 @@
 module Libm
 
-using Base: sign_mask, exponent_mask, significand_mask, exponent_one, exponent_bias, significand_bits
-
-using Base.Math: @horner
-
-export erf, erfc
+typealias FloatTypes Union{Float32,Float64}
 
 include("utils.jl")
 include("erf.jl")
-
-
 include("log/tang.jl")
 
 end
--- test/erf.jl
+++ test/erf.jl
@@ -1,31 +1,35 @@
 @testset "erf" begin
-    @test isnan(Libm.erf(NaN))
-    @test Libm.erf(Inf) == 1
-    @test Libm.erf(-Inf) == -1
-    s = linspace(-0.84375,0.84375,100)
-    @test_approx_eq Base.erf.(s) Libm.erf.(s)
-    s = linspace(-2e-28,2e-28,100)
-    @test_approx_eq Base.erf.(s) Libm.erf.(s)
-    s = linspace(-0.84375,0.84375,100)
-    @test_approx_eq Base.erf.(s) Libm.erf.(s)
-    s = linspace(0.84375, 6, 100)
-    @test_approx_eq Base.erf.(s) Libm.erf.(s)
-    s = linspace(-6,-0.84375, 100);
-    @test_approx_eq Base.erf.(s) Libm.erf.(s)
+    for T in (Float32,Float64)
+        @test isnan(Libm.erf(T(NaN)))
+        @test Libm.erf(T(Inf)) == 1
+        @test Libm.erf(T(-Inf)) == -1
+        s = linspace(T(-0.84375),T(0.84375),100)
+        @test_approx_eq Base.erf.(s) Libm.erf.(s)
+        s = linspace(T(-2e-28),T(2e-28),100)
+        @test_approx_eq Base.erf.(s) Libm.erf.(s)
+        s = linspace(T(-0.84375),T(0.84375),100)
+        @test_approx_eq Base.erf.(s) Libm.erf.(s)
+        s = linspace(T(0.84375), T(6), 100)
+        @test_approx_eq Base.erf.(s) Libm.erf.(s)
+        s = linspace(T(-6),T(-0.84375), 100);
+        @test_approx_eq Base.erf.(s) Libm.erf.(s)
+    end
 end
 
 @testset "erfc" begin
-    @test isnan(Libm.erfc(NaN))
-    @test Libm.erfc(Inf) == 0
-    @test Libm.erfc(-Inf) == 2
-    s = linspace(-0.84375,0.84375,100)
-    @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
-    s = linspace(-2e-56,2e-56,100)
-    @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
-    s = linspace(-0.25,0.25,100)
-    @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
-    s = linspace(0.84375, 28, 100)
-    @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
-    s = linspace(-28,-0.84375, 100);
-    @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
+    for T in (Float32,Float64)
+        @test isnan(Libm.erfc(T(NaN)))
+        @test Libm.erfc(T(Inf)) == 0
+        @test Libm.erfc(T(-Inf)) == 2
+        s = linspace(T(-0.84375),T(0.84375),100)
+        @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
+        s = linspace(T(-2e-56),T(2e-56),100)
+        @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
+        s = linspace(T(-0.25),T(0.25),100)
+        @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
+        s = linspace(T(0.84375), T(28), 100)
+        @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
+        s = linspace(T(-28),T(-0.84375), 100);
+        @test_approx_eq Base.erfc.(s) Libm.erfc.(s)
+    end
 end
