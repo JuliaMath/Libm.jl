f7d37054e66572c48fc5cc620c39bf379bf4bfc1
Mus M <mus-m@outlook.com>
2016-09-24 16:57:45 -0400

    Clean up hyp.jl and misc.jl files

--- src/Sleef/hyp.jl
+++ src/Sleef/hyp.jl
@@ -1,13 +1,13 @@
 # overflow argument value
-_max_val_sinhcosh(::Type{Float64}) = 710.0
-_max_val_sinhcosh(::Type{Float32}) = 89f0
+_over_sinhcosh(::Type{Float64}) = 710.0
+_over_sinhcosh(::Type{Float32}) = 89f0
 
 function xsinh{T<:FloatTypes}(x::T)
     y = abs(x)
     d = expk2(Double(y))
     d = ddsub(d, ddrec(d))
     y = (d.hi + d.lo) * T(0.5)
-    y = abs(x) > _max_val_sinhcosh(T) ? T(Inf) : y
+    y = abs(x) > _over_sinhcosh(T) ? T(Inf) : y
     y = isnan(y) ? T(Inf) : y
     y = flipsign(y, x)
     y = isnan(x) ? T(NaN) : y
@@ -19,15 +19,15 @@ function xcosh{T<:FloatTypes}(x::T)
     d = expk2(Double(y))
     d = ddadd(d, ddrec(d))
     y = (d.hi + d.lo) * T(0.5)
-    y = abs(x) > _max_val_sinhcosh(T) ? T(Inf) : y
+    y = abs(x) > _over_sinhcosh(T) ? T(Inf) : y
     y = isnan(y) ? T(Inf) : y
     y = isnan(x) ? T(NaN) : y
     return y
 end
 
 # overflow argument value
-_max_val_tanh(::Type{Float64}) = 18.714973875
-_max_val_tanh(::Type{Float32}) = 8.664339742f0
+_over_tanh(::Type{Float64}) = 18.714973875
+_over_tanh(::Type{Float32}) = 8.664339742f0
 
 function xtanh{T<:FloatTypes}(x::T)
     y = abs(x)
@@ -35,7 +35,7 @@ function xtanh{T<:FloatTypes}(x::T)
     e = ddrec(d)
     d = dddiv(ddsub(d, e), ddadd(d, e))
     y = d.hi + d.lo
-    y = abs(x) > _max_val_tanh(T) ? T(1.0) : y
+    y = abs(x) > _over_tanh(T) ? T(1.0) : y
     y = isnan(y) ? T(1.0) : y
     y = flipsign(y, x)
     y = isnan(x) ? T(NaN) : y
